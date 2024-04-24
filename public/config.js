73c869ffa83938759dc291b71ce5801cd7ecc88a
臻臻 <62086344+2644635282@users.noreply.github.com>
2024-04-24 11:12:33 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -2,35 +2,31 @@
 window.Config = {
 
   // 显示标题
-  SiteName: '云生站点监测',
+  SiteName: '服务监测',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'ur1078385-b9802a0868414b8a975d5038',
+    'ur2531205-3e8a4791331200dd4561704e',
   ],
 
     // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
   // 不过时间不要设置太长，容易卡，接口请求也容易失败
-  CountDays: 60,
+  CountDays: 30,
 
   // 是否显示检测站点的链接
   ShowLink: true,
 
   // 导航栏菜单
   Navi: [
-    {
-      text: '主页',
-      url: 'https://tjys.tk'
-    },
-    {
-      text: '博客',
-      url: 'https://blog.qikaile.tk'
-    },
-    {
-      text: 'GitHub',
-      url: 'https://github.com/qikaile/uptime-status'
-    },
+    // {
+     //  text: 'AI创作',
+    //   url: 'https://chatfriday.cn'
+    // },
+   //  {
+     //  text: 'AI创作-专业版',
+     //  url: 'https://blog.qikaile.tk'
+    // }
   ],
 };
